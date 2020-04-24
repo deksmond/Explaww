5c1c61bcd51a66dc439ad00d9feee143b6daae43
Desmond Amama <desmond@Desmonds-MacBook-Pro.local>
2020-04-24 04:03:22 +0100

    feat: TouchableOpacity used to test navigation feat: navigation page 'Page' created to test stack navigation

---
+++ ios/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+class page extends React.Component{
+    render(){
+        return(
+            <View style={{flex: 1}}>
+                <Text>This is another page</Text>
+            </View>
+        );
+    }
+}
+
+export default page;
