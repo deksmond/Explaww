7459b93fd91754c901c233d5db04748fb113dbf5
Desmond Amama <amamades@gmail.com>
2020-05-27 05:29:57 +0100

    style: material dropdown component integrated and used

--- ios/screens/ItemScreen.js
+++ ios/screens/ItemScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, Image, ScrollView, StyleSheet, SafeAreaView, Button, TouchableOpacity } from 'react-native';
+import { Text, View, Image, SafeAreaView, ScrollView, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'
+import { Dropdown } from 'react-native-material-dropdown'
 
 export default class ItemScreen extends Component {
   static navigationOptions = {
@@ -8,31 +9,46 @@ export default class ItemScreen extends Component {
   };
 
   render() {
-    return (    
+
+  let data = [{
+    value: 'Black',
+  },
+  {
+    value: 'Blue',
+  },
+  {
+  value: 'White'
+  }];
+
+    return (
           <ScrollView>
             <View>
-              <Image 
-                style={{width: '100%', height: '120%'}}
-                source={require('./img/vgs.jpg')}
-              />
+                <Image 
+                  style={{width: '100%', height: '100%'}}
+                  source={require('./img/vgs.jpg')}
+                />
 
-            <TouchableOpacity style={styles.backButton} onPress={() => this.props.navigation.navigate("Home")}>
-                <Icon name="ios-arrow-round-back" size={32} color="#FFF" />
-            </TouchableOpacity>
+              <TouchableOpacity style={styles.backButton} onPress={() => this.props.navigation.navigate("Home")}>
+                  <Icon name="ios-arrow-round-back" size={32} color="#FFF" />
+              </TouchableOpacity>
 
             </View>
 
             <View style={styles.text}>
-              <Text style={{ fontSize: 20, fontWeight: "700" }}>Dzd 4000</Text>
-              <Text style={{ fontSize: 18, paddingTop: 15 }}>Vans old school black semi rounded sunglasses</Text>
-              <Text style={{ fontSize: 17, paddingTop: 10 }}>Delivery fee: DZD 900</Text>
-           </View>
-
+                <Text style={{ fontSize: 20, fontWeight: "700" }}>Dzd 4000</Text>
+                <Text style={{ fontSize: 18, paddingTop: 15 }}>Vans old school black semi rounded sunglasses</Text>
+                  <Dropdown
+                    label='Colour'
+                    data={data}
+                  />
+                <Text style={{ fontSize: 17, paddingTop: 10 }}>Delivery fee: DZD 900</Text>
               <TouchableOpacity style={styles.button}>
                 <Text style={{color: '#FFF', textAlign:'center', fontSize: 20}}>BUY NOW</Text>
               </TouchableOpacity>
 
+            </View>
         </ScrollView>
+
     );
   }
 }
